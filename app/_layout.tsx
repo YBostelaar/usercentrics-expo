583799f32d9a9de13b9d085fa21dc731fab11972
ybostelaar <yanickbostelaar@gmail.com>
2025-11-25 19:20:29 +0100

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
+import {
+  Usercentrics,
+  UsercentricsOptions,
+} from '@usercentrics/react-native-sdk';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -12,11 +21,29 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
+  useEffect(() => {
+    const settingsId = 'cdgVkifUZn1zoY';
+    if (!settingsId) {
+      console.warn(
+        'UserCentrics settingsId is missing! Consent will not work without a valid settingsId.'
+      );
+      return;
+    }
+
+    const options = new UsercentricsOptions({
+      settingsId,
+    });
+    Usercentrics.configure(options);
+  }, []);
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: 'modal', title: 'Modal' }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
